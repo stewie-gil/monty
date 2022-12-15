6a980e403cc3f963a33da3dcd6a8b94035492582
stewie-gil <gilliganstewie7@gmail.com>
2022-12-15 15:17:53 -0800

    m

--- 28.m
+++
@@ -1,2 +0,0 @@
-push 150
-pchar
--- main.c
+++ main.c
@@ -1,27 +1,47 @@
 #include "monty.h"
 
+vars var;
+
 /**
- * main - entry point of our monty program
- * @argc: the amount of arguments given to the program
- * @argv: the array of arguments given to the program
- *
- * Return: EXIT_SUCCESS if no error has occures, otherwise EXIT_FAILURE
+ * main - Start LIFO, FILO program
+ * @ac: Number of arguments
+ * @av: Pointer containing arguments
+ * Return: 0 Success, 1 Failed
  */
-
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
-	FILE *fp;
+	char *opcode;
 
-	META.error = 0;
-	META.state = STACK;
-
-	if (handle_input(argc, argv, &fp) == 1 || monty_run(fp) == 1)
+	if (ac != 2)
 	{
-		if (fp)
-			fclose(fp);
-		exit(EXIT_FAILURE);
+		fprintf(stderr, "USAGE: monty file\n");
+		return (EXIT_FAILURE);
 	}
 
-	fclose(fp);
+	if (start_vars(&var) != 0)
+		return (EXIT_FAILURE);
+
+	var.file = fopen(av[1], "r");
+	if (!var.file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
+		free_all();
+		return (EXIT_FAILURE);
+	}
+
+	while (getline(&var.buff, &var.tmp, var.file) != EOF)
+	{
+		opcode = strtok(var.buff, " \r\t\n");
+		if (opcode != NULL)
+			if (call_funct(&var, opcode) == EXIT_FAILURE)
+			{
+				free_all();
+				return (EXIT_FAILURE);
+			}
+		var.line_number++;
+	}
+
+	free_all();
+
 	return (EXIT_SUCCESS);
 }
